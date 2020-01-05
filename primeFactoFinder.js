e94c104d404c66499caf72bb45262c701542ff71
charlie-wild <charliewild@gmail.com>
2020-01-05 17:20:38 +0000

    adding more tests - unusual error code seen

--- primeFactoFinder.js
+++ primeFactoFinder.js
@@ -3,19 +3,43 @@
 What is the largest prime factor of the number 600851475143 ? */
 
 
-const highPrimeFactorFinder = (targetNum) => {
-    
+const highPrimeFactorFinder = (number) => {
+    return primeFactorFinder(number).sort(function(a, b) {
+      return b - a;
+    })[0];
   }
 
 
 const primeFactorFinder = (number) => {
-  let primeFactorArr = [];
+  let primeArr = primeFinder(number);
+  return primeArr.filter(n => number%n ===0);    
 
 }
 
-const primeFinder = (upperLimit) => {
-    let primes = [];
 
-}
+const primeFinder = (max) => {
+        const sieve = new Array(max).fill(true);
+
+      // Iterate from 2 until square root of max
+      for (let i = 2; i < Math.sqrt(max); i++) {
+        // If the number is labelled a prime then we can start at i^2 and mark every multiple of i
+        // from there as NOT a prime
+        if (sieve[i]) {
+          for (let j = Math.pow(i, 2); j < max; j += i) {
+            sieve[j] = false;
+          }
+        }
+      }
+
+      // Now we can reduce our sieve to only the Prime indexes that are true
+      return sieve.reduce((primes, isPrime, i) => {
+        if (isPrime && i > 1) {
+          primes.push(i);
+        }
+
+        return primes;
+      }, []);
+    };
+
 
 module.exports = { highPrimeFactorFinder }
--- spec/primeFactorFinder.spec.js
+++ spec/primeFactorFinder.spec.js
@@ -5,4 +5,10 @@ describe('highPrimeFactorFinder()', () => {
   it('returns 5 when the input is 25', () => {
     expect(highPrimeFactorFinder(25)).to.equal(5);
   });
-});
+  it('returns 23 when the input is 345', () => {
+    expect(highPrimeFactorFinder(345)).to.equal(23);
+  });
+  it("returns 6857 when the input is 600851475143", () => {
+    expect(highPrimeFactorFinder(600851475143)).to.equal(6857);
+  });
+})
