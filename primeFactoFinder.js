0de8f4ad013b5837a5ad934daf595c007e75c231
charlie-wild <charliewild@gmail.com>
2020-01-05 15:11:01 +0000

    skeleton for finding primes

--- primeFactoFinder.js
+++ primeFactoFinder.js
@@ -4,7 +4,18 @@ What is the largest prime factor of the number 600851475143 ? */
 
 
 const highPrimeFactorFinder = (targetNum) => {
-
+    
   }
+
+
+const primeFactorFinder = (number) => {
+  let primeFactorArr = [];
+
+}
+
+const primeFinder = (upperLimit) => {
+    let primes = [];
+
+}
 
 module.exports = { highPrimeFactorFinder }
