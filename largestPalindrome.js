45afc330211e9e39ffeb78844485c7f7640750a8
charlie-wild <charliewild@gmail.com>
2020-01-05 19:28:46 +0000

    working variant for palindrome finder but needs to be refactored

---
+++ largestPalindrome.js
@@ -0,0 +1,33 @@
+// A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+
+// Find the largest palindrome made from the product of two 3-digit numbers.
+
+
+
+const largePalindromeFinder = () => {
+  let result = 0;
+  for (let i = 999; i > 100; i--) {
+    for (let j = 999; j > 100; j--) {
+      let num = i*j;
+      let numArr = num.toString().split('');
+      let reverseNum = parseInt(numArr.reverse().join(''));
+       if (num == reverseNum) {
+        result = num;
+        break;
+      }
+      if (result) break;
+    }
+    if (result) break;
+  }
+    return result;
+  
+}
+
+console.log(largePalindromeFinder());
+
+module.exports = { largePalindromeFinder }
+
+
+
+
+
