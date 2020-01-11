70974cf564ca60625e2107c5d66cd1bf72a30c10
charlie-wild <charliewild@gmail.com>
2020-01-11 16:38:07 +0000

    prime finder utility function added

--- solutions/smallestMultiplier.js
+++ solutions/smallestMultiplier.js
@@ -2,10 +2,34 @@
 
 // What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20 ?
 
-const {primeFinder} = require('./primeFactoFinder');
+const {primeFinder} = require('../utility/primeFinder');
 
 // use primeFinder to get an array of all the necessary divisors
 
-const multiples = primeFinder(20);
+
+const smallestMultiplier = () => {
+  const multiples = primeFinder(20); //array
+
+  let answer = 0;
+  let found = false;
+ 
+  for (let i = 20; i < 10000; i++) {   
+    for (let j = 0; j < multiples.length; j++) {
+      if (i%multiples[j]===0) {
+        found = true;
+      } else {
+        found = false;
+      }
+    }
+    if (found) {
+      answer = i;
+      break;
+    }
+  }
+  return answer;
+}
+
+console.log(smallestMultiplier());
+
 
 
